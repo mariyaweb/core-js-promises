193ab570d2f64681315f61f220b27d245fb92b53
github-mariyaweb <96mariyask@gmail.com>
2024-04-09 18:19:17 +0300

    feat: add 7 task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -136,8 +136,14 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let res = Promise.resolve('');
+  promises.forEach((promise) => {
+    res = res.then((prevStr) =>
+      promise.then((currStr) => prevStr + currStr.toString())
+    );
+  });
+  return res;
 }
 
 module.exports = {
